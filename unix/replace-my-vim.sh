c8a06e880c6af6c574962d91cd262a7d7fd81a45
elleryq <elleryq@gmail.com>
2014-08-05 14:53:52 +0800

    Fix path error.

--- unix/replace-my-vim.sh
+++ unix/replace-my-vim.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 
-ln -s ./dist/ctags_lang ~/.ctags
-ln -s .vimrc ~/.vimrc
-ln -s .vimrc.plugins ~/.vimrc.plugins
-ln -s .vimrc.local ~/.vimrc.local
-ln -s .vimrc.plugins.local ~/.vimrc.plugins.local
+PWD=$(pwd)
+ln -s $PWD/dist/ctags_lang ~/.ctags
+ln -s $PWD/.vimrc ~/.vimrc
+ln -s $PWD/.vimrc.plugins ~/.vimrc.plugins
+ln -s $PWD/.vimrc.local ~/.vimrc.local
+ln -s $PWD/.vimrc.plugins.local ~/.vimrc.plugins.local
 rm -rf ~/.vim
-ln -s vimfiles ~/.vim
+ln -s $PWD/vimfiles ~/.vim
